9419237536a95b13d60081dbefaecc1d98073cf6
RobertLemos14 <95971607+RobertLemos14@users.noreply.github.com>
2022-04-15 20:53:38 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -3,6 +3,6 @@ salario = input("Digite seu salario de Abril, porfavor")
 if (isinstance(nome, str) and isinstance(salario,int)):
     print("o salario de ", nome, " foi de R$", salario[0 : 4], ",", salario[4 : 6], " reais", sep='')
 else:
-    print("Você digitou um nome no lugar do numero ou o contrario tente de novo")
+    print("Você digitou um nome no lugar do numero ou o contrario, tente de novo")
 
 
